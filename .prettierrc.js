0b648f9d6981bb343ea302a407630661ccc10ed1
Avijit Saha <jit.avi.saha2997@gmail.com>
2024-06-19 22:06:11 +0600

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  singleQuote: true,
+  tabWidth: 2,
+  semi: true,
+  jsxSingleQuote: true,
+  printWidth: 80,
+  arrowParens: 'avoid',
+  bracketSameLine: true,
+  bracketSpacing: false,
+  singleQuote: true,
+  trailingComma: 'all',
+};
---
+++ App.tsx
@@ -0,0 +1,76 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react';
+import {FlatList, SectionList, StyleSheet, Text, View} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+  },
+});
+
+const FlatListBasics = () => {
+  return (
+    <View style={styles.container}>
+      {/* <FlatList
+        data={[
+          {key: 'Devin'},
+          {key: 'Dany'},
+          {key: 'Dominic'},
+          {key: 'Jackson'},
+          {key: 'James'},
+          {key: 'Joel'},
+          {key: 'John'},
+          {key: 'Jillian'},
+          {key: 'Jimmy'},
+          {key: 'Julie'},
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      /> */}
+      <SectionList sections={[
+        {title: 'D', data: ['Devin', 'Dany', 'Dominic']},
+        {
+          title: 'J',
+          data: [
+            'Jackson',
+            'James',
+            'Jillian',
+            'Jimmy',
+            'Joel',
+            'John',
+            'Julie',
+          ],
+        },
+        {
+          title: 'k',
+          data: [
+            'Kenny',
+            'kane',
+          ],
+        },
+      ]}
+      renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+      renderSectionHeader={({section}) => (
+        <Text style={styles.sectionHeader}>{section.title}</Text>
+      )}
+      />
+    </View>
+  );
+};
+
+export default FlatListBasics;
